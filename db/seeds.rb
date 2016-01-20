2886ab26040f182ee8d23977c59a0594b8755d6f
Jacky <jackylai87@gmail.com>
2016-01-20 17:18:10 +0800

    Remove seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
-# @values = File.open("db/urls.txt")
-b = Array.new
-values = File.open("db/urls.txt")
+# # @values = File.open("db/urls.txt")
+# b = Array.new
+# values = File.open("db/urls.txt")
 
-values.each do |url|
-	a = url.chomp.split(//)
-	a.delete("(")
-	a.delete(")")
-	a.delete(",")
-	b << a.join
-	# b.each do |url|
-	# 	Url.create(long_url: url)
-	# end
-end
+# values.each do |url|
+# 	a = url.chomp.split(//)
+# 	a.delete("(")
+# 	a.delete(")")
+# 	a.delete(",")
+# 	b << a.join
+# 	# b.each do |url|
+# 	# 	Url.create(long_url: url)
+# 	# end
+# end
 
 
-Url.transaction do
-	b.each do |x|
-		c = SecureRandom.hex(3)
-  	Url.connection.execute "INSERT INTO urls (long_url, short_url, created_at, updated_at) VALUES ('#{x}','#{c}','#{Time.now}','#{Time.now}')"
-	end
-end
+# Url.transaction do
+# 	b.each do |x|
+# 		c = SecureRandom.hex(3)
+#   	Url.connection.execute "INSERT INTO urls (long_url, short_url, created_at, updated_at) VALUES ('#{x}','#{c}','#{Time.now}','#{Time.now}')"
+# 	end
+# end
